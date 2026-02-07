e0055ecaf13eff5ca329c4ac4b9f7ec5394f1bed
rubenstr0y <rubenk17mm@gmail.com>
2026-02-07 23:39:26 +0100

    Add Audio Bark to chase Ball

--- scripts/dog/chase_ball_state.gd
+++ scripts/dog/chase_ball_state.gd
@@ -5,7 +5,7 @@ extends Dog_State
 @export var interact_player_state: Dog_State
 
 func enter() -> void:
-	pass
+	AudioManager.create_2d_audio_at_location(parent.position,SoundEffect.SOUND_EFFECT_TYPE.DOG_BARK)
 
 func process_frame(delta: float) -> Dog_State:
 	
