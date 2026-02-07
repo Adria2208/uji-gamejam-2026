40b58829c0b6a41a43cf290c9ebfc49c4a08c7ff
rubenstr0y <rubenk17mm@gmail.com>
2026-02-07 21:15:53 +0100

    Chase, Search and Wait dog states

--- scripts/dog/chase_ball_state.gd
+++ scripts/dog/chase_ball_state.gd
@@ -7,5 +7,14 @@ extends Dog_State
 func enter() -> void:
 	pass
 
-func process_physics(delta: float) -> Dog_State:
-	return null
+func process_frame(delta: float) -> Dog_State:
+	
+	if Input.is_action_just_pressed("interact") and !parent.interacting_with_player:
+		return interact_player_state
+	
+	parent.position = parent.position.move_toward(parent.closest_ball.position,delta * parent.dog_speed)
+	
+	if abs(parent.closest_ball.position - parent.position) < Vector2(10.0,10.0):
+		return bite_state
+	else:
+		return null
